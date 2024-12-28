2a16c06fbf96b54570e738a2f173564ccd28b365
AlexKnauth <alexander@knauth.org>
2024-12-28 15:55:21 -0500

    class, sync, docstring

--- src/MatoStreamshow.py
+++ src/MatoStreamshow.py
@@ -1,11 +1,19 @@
 import config
 import discord
-from discord.ext import commands
+from discord import app_commands
+
+class MatoStreamshow(discord.Client):
+    def __init__(self, *, intents: discord.Intents) -> None:
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        await self.tree.sync()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = MatoStreamshow(intents=intents)
 
 @bot.event
 async def on_ready():
@@ -13,12 +21,19 @@ async def on_ready():
 
 @bot.tree.command()
 async def ping(interaction: discord.Interaction):
+    """
+    Replies to ping with pong.
+
+    Parameters
+    ----------
+    interaction : discord.Interaction
+        The interaction object.
+    """
     await interaction.response.send_message("Pong!")
 
 def main():
     if config.token == "":
         raise ValueError('config token not found')
-    bot.tree.sync()
     bot.run(config.token)
 
 if __name__ == "__main__":
